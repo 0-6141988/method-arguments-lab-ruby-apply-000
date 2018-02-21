dd5696a3f4072bb08e3a2c1d2b37912c9277b861
Julian Driver <ijulian.driver@gmail.com>
2018-02-21 23:59:18 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,6 @@ end
 
 # introduction_with_language.rb 
 
-def introduction_with_language 
-puts ("Hi my name is Dan and I am learning to program in #{language})
+def introduction_with_language  (name, language)
+  puts ("Hi my name is #{name} and I am learning to program in #{language})
 end
