6b8573cdda21065cab7173eb11a26af94618f82c
Julian Driver <ijulian.driver@gmail.com>
2018-02-21 23:45:36 +0000

    automatically backed up by learn

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+# lib/introduction.rb
+
+def introduction (name) puts "Hi, my name is Dan."
+end
+
+# introduction_with_language.rb 
+
+def introduction_with_language puts ("Hi my name is Dan and I am learning to program in Ember.js.\n").to_stdout")
+end
